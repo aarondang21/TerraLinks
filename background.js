e1ec095a3dfeefe3e07449ae2a47872fa46b2321
Marc Helou <marchelou@Marcs-Air.home>
2020-07-23 18:27:52 -0400

    Designated two separate html files for links found/not found

--- background.js
+++ background.js
@@ -1,5 +1,21 @@
 // background.js
+// *IMPORTANT* Use:
+// chrome.storage.local.set({'links_found': true});
+// For when we find links in the algorithm
 chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+    chrome.storage.local.get('links_found', function(data) {
+      if (data.links_found) {
+        chrome.pageAction.setPopup({
+          tabId: tabId,
+          popup: 'popup.html'
+        });
+      } else {
+        chrome.browserAction.setPopup({
+          tabId: tabId,
+          popup: 'popup_sign_in.html'
+        });
+      }
+      });
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
         let url = tabs[0].url;
         if (url.includes("https://www.openstreetmap.org/way/") || url.includes("https://www.openstreetmap.org/node/")) {
