c6b0acd0dbb9891aa60c1334a863014934bc2986
Aaron Dang <aarondang1111@gmail.com>
2020-07-28 02:10:29 -0400

    search function & tester

--- scripts/search.js
+++ scripts/search.js
@@ -1,11 +1,27 @@
-var url = "https://en.wikipedia.org/w/api.php";
+function searchFunc() {
 
-var params = {
-    action: "query",
-    list: "search",
-    srsearch: "Washington Monument",
-    format: "json"
-};
+    var url = "https://en.wikipedia.org/w/api.php";
 
-url = url + "?origin=*";
-Object.keys(params).forEach(function (key) { url += "&" + key + "=" + params[key]; });
+    var data = document.getElementById("name").value;
+
+    var params = {
+        action: "query",
+        list: "search",
+        srsearch: data,
+        format: "json"
+    };
+
+    url = url + "?origin=*";
+    Object.keys(params).forEach(function (key) { url += "&" + key + "=" + params[key]; });
+
+    fetch(url)
+        .then(function (response) { return response.json(); })
+        .then(function (response) {
+            var pages = response.query.search;
+            for (var place in pages) {
+                console.log(pages[place].title);
+                console.log("http://en.wikipedia.org/?curid=" + pages[place].pageid);
+            }
+        })
+        .catch(function (error) { console.log(error); });
+}
