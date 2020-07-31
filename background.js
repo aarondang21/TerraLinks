d01f3f7dc2cac5de761f5026ce53b63459bf01f8
marc-helou <marcerino15@gmail.com>
2020-07-30 22:26:26 -0400

    implemented OSM data extraction through OverPass, removed jQuery

--- background.js
+++ background.js
@@ -2,37 +2,48 @@
 // *IMPORTANT* Use:
 // chrome.storage.local.set({'links_found': true});
 // For when we find links in the algorithm
-chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-    chrome.storage.local.get('links_found', function(data) {
-      if (data.links_found) {
-        chrome.pageAction.setPopup({
-          tabId: tabId,
-          popup: 'popup.html'
-        });
-      } else {
-        chrome.pageAction.setPopup({
-          tabId: tabId,
-          popup: 'popup_nolinks.html'
-        });
-      }
-      });
-    chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-        let url = tabs[0].url;
-        if (url.includes("https://www.openstreetmap.org/way/") || url.includes("https://www.openstreetmap.org/node/")
-        || url.includes("https://www.openstreetmap.org/relation/")) {
-            alert("You are at an OSM Location!");
+chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
+  if (changeInfo.status === 'complete') {
+    chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+      let url = tabs[0].url;
+      if ((url.includes("https://www.openstreetmap.org/way/") || url.includes("https://www.openstreetmap.org/node/")
+        || url.includes("https://www.openstreetmap.org/relation/")) && !(url.includes("#"))) {
+        var pathArray = url.split('/'); // splits URL by slashes
+        var locType = pathArray[3]; // way, relation or node
+        var osm_id = pathArray[4]; // unique location identifier in OSM
+        var queryUrl = `https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];${locType}(${osm_id});out tags bb;`;
+        const Http = new XMLHttpRequest();
+        Http.open("GET", queryUrl);
+        Http.send();
+        Http.onreadystatechange = (e) => {
+          console.log(Http.responseText);
         }
+      }
     });
-});
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.local.set({'links_found': false}); // move this somewhere else later
-    chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-      chrome.declarativeContent.onPageChanged.addRules([{
-        conditions: [new chrome.declarativeContent.PageStateMatcher({
-          pageUrl: {hostEquals: 'www.openstreetmap.org'},
-        })
-        ],
-            actions: [new chrome.declarativeContent.ShowPageAction()]
-      }]);
-    });
+  }
+  chrome.storage.local.get('links_found', function (data) {
+    if (data.links_found) {
+      chrome.pageAction.setPopup({
+        tabId: tabId,
+        popup: 'popup.html'
+      });
+    } else {
+      chrome.pageAction.setPopup({
+        tabId: tabId,
+        popup: 'popup_nolinks.html'
+      });
+    }
   });
+});
+chrome.runtime.onInstalled.addListener(function () {
+  chrome.storage.local.set({ 'links_found': false }); // move this somewhere else later
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+    chrome.declarativeContent.onPageChanged.addRules([{
+      conditions: [new chrome.declarativeContent.PageStateMatcher({
+        pageUrl: { hostEquals: 'www.openstreetmap.org' },
+      })
+      ],
+      actions: [new chrome.declarativeContent.ShowPageAction()]
+    }]);
+  });
+});
