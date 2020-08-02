1aa51b3f95573ce04fd3b70786ed2b2b60f621bb
marc-helou <marcerino15@gmail.com>
2020-08-02 15:22:51 -0400

    Basic algorithm implemented

--- background.js
+++ background.js
@@ -4,6 +4,7 @@
 // For when we find links in the algorithm
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (changeInfo.status === 'complete') {
+    chrome.storage.local.set({ 'links_found': false });
     chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
       let url = tabs[0].url;
       if ((url.includes("https://www.openstreetmap.org/way/") || url.includes("https://www.openstreetmap.org/node/")
@@ -11,48 +12,63 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
         var pathArray = url.split('/'); // splits URL by slashes
         var locType = pathArray[3]; // way, relation or node
         var osm_id = pathArray[4]; // unique location identifier in OSM
-        var queryUrl = `https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];${locType}(${osm_id});out tags bb;`;
-        const req = new XMLHttpRequest();
-        req.open("GET", queryUrl);
-        req.responseType = 'json';
-        req.send();
         var osmData; // stores OSM data in JSON format
         var lat; // latitude
         var lon; // longitude
         var name // location's name
-        req.onreadystatechange = (e) => {
-          osmData = req.response;
-          console.log(osmData);
-        }
-        // The following lines gather name, longitude and latidude from OSM JSON
-        name = osmData.elements['0'].tags['name:en']; // fix this
-        if(locType == 'way' || locType == 'relation') {
-          lat = (osmData.minlat + osmData.maxlat) / 2;
-          lon = (osmData.minlon + osmData.maxlon) / 2;
-        }
-        else {
-          lat = osmData.lat;
-          lon = osmData.lon;
+        var queryUrl = `https://overpass-api.de/api/interpreter?data=[out:json][timeout:25];${locType}(${osm_id});out tags bb;`;
+        console.log("Querying Overpass... this may take a while");
+        const req = new XMLHttpRequest();
+        req.open("GET", queryUrl, true);
+        req.responseType = 'json';
+        req.send();
+        req.onreadystatechange = function () {
+          if (this.readyState == 4 && this.status == 200) {
+            osmData = req.response;
+            console.log(osmData);
+            // The following lines gather name, longitude and latidude from OSM JSON
+            name = osmData.elements[0].tags['name:en'];
+            console.log(name);
+            if (name != null) {
+              if (locType == 'way' || locType == 'relation') {
+                lat = (osmData.elements[0].bounds.minlat + osmData.elements[0].bounds.maxlat) / 2;
+                console.log(lat);
+                lon = (osmData.elements[0].bounds.minlon + osmData.elements[0].bounds.maxlon) / 2;
+                console.log(lon);
+              }
+              else {
+                lat = osmData.elements[0].lat;
+                console.log(lat);
+                lon = osmData.elements[0].lon;
+                console.log(lon);
+              }
+              chrome.storage.local.set({ 'links_found': geoFunc(lat, lon, name) });
+            }
+            else {
+              console.log("This is not a named location!");
+            }
+          }
         }
       }
     });
   }
+});
+chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
   chrome.storage.local.get('links_found', function (data) {
     if (data.links_found) {
       chrome.pageAction.setPopup({
-        tabId: tabId,
+        tabId: tabs[0].id,
         popup: 'popup.html'
       });
     } else {
       chrome.pageAction.setPopup({
-        tabId: tabId,
+        tabId: tabs[0].id,
         popup: 'popup_nolinks.html'
       });
     }
   });
 });
 chrome.runtime.onInstalled.addListener(function () {
-  chrome.storage.local.set({ 'links_found': false }); // move this somewhere else later
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([{
       conditions: [new chrome.declarativeContent.PageStateMatcher({
