8739ca72abbce5b90fb313825c6877852e02dd35
Marc Helou <marchelou@Marcs-Air.home>
2020-08-02 15:22:51 -0400

    Basic algorithm implemented

--- scripts/geosearch.js
+++ scripts/geosearch.js
@@ -1,14 +1,6 @@
-function geoFunc() {
+function geoFunc(latitude, longitude, placeName) {
 
-    var coordinates = document.getElementById("coords");
-    //console.log(coordinates.elements[0].value);
-    //console.log(coordinates.elements[1].value);
-
-    var lat = coordinates.elements[0].value;
-    var long = coordinates.elements[1].value;
-    let position = lat + "|" + long;
-
-    //console.log(position);
+    let position = latitude + "|" + longitude;
 
     var url = "https://en.wikipedia.org/w/api.php";
 
@@ -19,21 +11,24 @@ function geoFunc() {
         gsradius: "10000",
         gslimit: "10",
         format: "json",
-    }; 
+    };
 
     url = url + "?origin=*";
     Object.keys(params).forEach(function (key) { url += "&" + key + "=" + params[key]; });
-
-    //console.log(url);
 
     fetch(url)
         .then(function (response) { return response.json(); })
         .then(function (response) {
             var pages = response.query.geosearch;
             for (var place in pages) {
-                console.log(pages[place].title);
-                console.log("http://en.wikipedia.org/?curid=" + pages[place].pageid);
+                name = pages[place].title;
+                if (name == placeName) {
+                    console.log("http://en.wikipedia.org/?curid=" + pages[place].pageid);
+                    return true;
+                }
             }
+            console.log("No links match!");
+            return false;
         })
         .catch(function (error) { console.log(error); });
 }
