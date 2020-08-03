7b957912b5a49a8aabe9e151300967b2d751c408
marc-helou <marcerino15@gmail.com>
2020-08-02 20:21:23 -0400

    refined geosearch and search, making algorithm perform better in cities and large places

--- scripts/search.js
+++ scripts/search.js
@@ -1,27 +1,47 @@
-function searchFunc() {
+function searchFunc(latitude, longitude, placeName) {
 
     var url = "https://en.wikipedia.org/w/api.php";
-
-    var data = document.getElementById("name").value;
 
     var params = {
         action: "query",
         list: "search",
-        srsearch: data,
+        srsearch: placeName,
         format: "json"
     };
 
     url = url + "?origin=*";
     Object.keys(params).forEach(function (key) { url += "&" + key + "=" + params[key]; });
 
-    fetch(url)
-        .then(function (response) { return response.json(); })
-        .then(function (response) {
-            var pages = response.query.search;
+    const data = new XMLHttpRequest();
+    data.open("GET", url, true);
+    data.responseType = 'json';
+    data.send();
+    return data.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var pages = data.response.query.search;
             for (var place in pages) {
-                console.log(pages[place].title);
-                console.log("http://en.wikipedia.org/?curid=" + pages[place].pageid);
+                name = pages[place].title;
+                id = pages[place].pageid;
+                url2 = `https://en.wikipedia.org/w/api.php?action=query&prop=coordinates&titles=${name}&format=json`
+                const data2 = new XMLHttpRequest();
+                data2.open("GET", url2, true);
+                data2.responseType = 'json';
+                data2.send();
+                return data2.onreadystatechange = function (id) {
+                    if (this.readyState == 4 && this.status == 200) {
+                        keys = Object.keys(id.currentTarget.response.query.pages);
+                        id = keys[0];
+                        if (Object.keys(data2.response.query.pages[id]).includes("coordinates")) {
+                            lat = data2.response.query.pages[id].coordinates[0].lat;
+                            lon = data2.response.query.pages[id].coordinates[0].lon;
+                            if ((lat - latitude <= 0.1 && lat - latitude >= -0.1) || lon - longitude <= 0.1 && lon - longitude <= -0.1) {
+                                console.log("http://en.wikipedia.org/?curid=" + id);
+                                return true;
+                            }
+                        }
+                    }
+                }
             }
-        })
-        .catch(function (error) { console.log(error); });
+        }
+    }
 }
