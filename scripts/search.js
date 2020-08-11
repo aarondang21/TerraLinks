17897550ca195533dee1c3876c94ef39959be22f
Marc Helou <marchelou@Marcs-Air.home>
2020-08-11 19:19:56 -0400

    refined entire algorithm; better async use; includes osm wiki links; multi-lang support

--- scripts/search.js
+++ scripts/search.js
@@ -1,10 +1,11 @@
-function searchFunc(latitude, longitude, placeName) {
+async function searchFunc(latitude, longitude, placeName) {
 
     var url = "https://en.wikipedia.org/w/api.php";
 
     var params = {
         action: "query",
         list: "search",
+        srlimit: 50,
         srsearch: placeName,
         format: "json"
     };
@@ -12,36 +13,31 @@ function searchFunc(latitude, longitude, placeName) {
     url = url + "?origin=*";
     Object.keys(params).forEach(function (key) { url += "&" + key + "=" + params[key]; });
 
-    const data = new XMLHttpRequest();
-    data.open("GET", url, true);
-    data.responseType = 'json';
-    data.send();
-    return data.onreadystatechange = function () {
-        if (this.readyState == 4 && this.status == 200) {
-            var pages = data.response.query.search;
-            for (var place in pages) {
-                name = pages[place].title;
-                id = pages[place].pageid;
-                url2 = `https://en.wikipedia.org/w/api.php?action=query&prop=coordinates&titles=${name}&format=json`
-                const data2 = new XMLHttpRequest();
-                data2.open("GET", url2, true);
-                data2.responseType = 'json';
-                data2.send();
-                return data2.onreadystatechange = function (id) {
-                    if (this.readyState == 4 && this.status == 200) {
-                        keys = Object.keys(id.currentTarget.response.query.pages);
-                        id = keys[0];
-                        if (Object.keys(data2.response.query.pages[id]).includes("coordinates")) {
-                            lat = data2.response.query.pages[id].coordinates[0].lat;
-                            lon = data2.response.query.pages[id].coordinates[0].lon;
-                            if ((lat - latitude <= 0.1 && lat - latitude >= -0.1) || lon - longitude <= 0.1 && lon - longitude <= -0.1) {
-                                console.log("http://en.wikipedia.org/?curid=" + id);
-                                return true;
-                            }
-                        }
-                    }
-                }
+    console.log("Querying Wikipedia...");
+    let response = await fetch(url);
+    let data = await response.json();
+    var pages = data.query.search;
+    var status = false;
+    
+    for (var place in pages) {
+        name = pages[place].title;
+        id = pages[place].pageid;
+        url2 = `https://en.wikipedia.org/w/api.php?action=query&prop=coordinates&titles=${name}&format=json`
+        let response2 = await fetch(url2);
+        let data2 = await response2.json();
+        console.log(data2.query.pages[id]);
+        if (Object.keys(data2.query.pages[id]).includes("coordinates")) {
+            lat = data2.query.pages[id].coordinates[0].lat;
+            lon = data2.query.pages[id].coordinates[0].lon;
+            if ((lat - latitude <= 0.5 && lat - latitude >= -0.5) && (lon - longitude <= 0.5 && lon - longitude >= -0.5)) {
+                console.log("http://en.wikipedia.org/?curid=" + id);
+                status = true;
+                break;
             }
         }
     }
+    if(!status) {
+        console.log("No links found!");
+    }
+    return status;
 }
