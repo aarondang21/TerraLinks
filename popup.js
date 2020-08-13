6c78766e22df31b453cfdee81569c6fa17c2ac6f
Marc Helou <marchelou@Marcs-Air.home>
2020-08-12 20:12:18 -0400

    testing div addition

--- popup.js
+++ popup.js
@@ -1,11 +1,17 @@
 // popup.js
-chrome.storage.local.get(['href1'], function (result) {
+chrome.storage.local.get(['href1'], function(result) {
     if (result.href1 != null) {
-        // add div
+        let div = document.createElement('div');
+        div.id = 'algLink';
+        div.innerHTML = `<a href="${result.href1}>link text</a>';`
+        document.body.appendChild(div);
     }
 });
-chrome.storage.local.get(['href2'], function (result) {
+chrome.storage.local.get(['href2'], function(result) {
     if (result.href2 != null) {
-        // add div
+        let div = document.createElement('div');
+        div.id = 'directLink';
+        div.innerHTML = `<a href="${result.href2}>link text</a>';`
+        document.body.appendChild(div);
     }
 });
