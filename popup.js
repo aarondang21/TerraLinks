3801281b510dfe594b4e4b5d07a697716d8b4052
marc-helou <marcerino15@gmail.com>
2020-08-12 20:07:05 -0400

    initial UI changes

--- popup.js
+++ popup.js
@@ -1,1 +1,11 @@
 // popup.js
+chrome.storage.local.get(['href1'], function (result) {
+    if (result.href1 != null) {
+        // add div
+    }
+});
+chrome.storage.local.get(['href2'], function (result) {
+    if (result.href2 != null) {
+        // add div
+    }
+});
